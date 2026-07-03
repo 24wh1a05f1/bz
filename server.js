a1a788b8979ec0227ee7a34c159532be0001df85
sony <24wh1a05f1@bvrithyderabad.edu.in>
2026-07-03 14:59:21 +0530

    initial  commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3002;
+//web server
+app.use(express.static('frontend'))
+app.listen(port, function(){
+    console.log(" successfully running at http://location:"+port)
+   
+})
+
+
