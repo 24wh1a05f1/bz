abd17459d61f2b7287eef94929ee0537c12ba66e
sony <24wh1a05f1@bvrithyderabad.edu.in>
2026-07-05 10:24:12 +0530

    home-page

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3002;
-//web server
-app.use(express.static('frontend'))
-app.listen(port, function(){
-    console.log(" successfully running at http://location:"+port)
-   
-})
+const express = require("express");
+const path = require("path");
 
+const app = express();
+const PORT = 3002;
 
+app.use(express.static(path.join(__dirname, "frontend")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
+console.log(__dirname);
+console.log(path.join(__dirname, "frontend", "index.html"));
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
